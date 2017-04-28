3e490b962e67037865890f70047f001087f0cff5
Juliana <julianahafid@gmail.com>
2017-04-28 23:59:04 +0800

    create game logic

--- JS/main.js
+++ JS/main.js
@@ -9,19 +9,42 @@ console.log("User flipped " + cardThree);**/
 
 var cards = ["queen","queen","king","king"] ;
 var cardsInPlay = [] ;
-var cardOne = cards[0] ;
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
 
-if (cardsInPlay.length === 2)
-{
+var checkForMatch = function (){
+
 	if (cardsInPlay[0] === cardsInPlay[1])
 	{
-		alert ("You found a match!");
+		console.log("You found a match!");
 	}
 	else
-		alert("Sorry, try again.")
-}
+	{
+		console.log("Sorry, try again.")
+	}
+		
+};
+var flipcard = function (cardId){
+	/*var cardOne = cards[0] ;
+	cardsInPlay.push(cardOne);
+	console.log("User flipped " + cardOne);
+	var cardTwo = cards[2];
+	cardsInPlay.push(cardTwo);
+	console.log("User flipped " + cardTwo);*/
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push (cards[cardId]);
+	checkForMatch ();
+	/*if (cardsInPlay.length === 2)
+	{
+		if (cardsInPlay[0] === cardsInPlay[1])
+		{
+			alert ("You found a match!");
+		}
+		else
+			alert("Sorry, try again.")
+		}
+	}*/
+}; // end flipCard function
+
+
+flipcard (0);
+flipcard (2);
+
