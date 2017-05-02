42e16d9b3a5de2282fd7d783804eeacad56a660b
Juliana <julianahafid@gmail.com>
2017-05-03 00:52:46 +0800

    created game logic

--- JS/main.js
+++ JS/main.js
@@ -30,37 +30,80 @@ var cards = [
 	}
 ];
 
-
 var cardsInPlay = [] ;
+
+var score = 0;
 
 //start checkForMatch function
 var checkForMatch = function (){
-	if (cardsInPlay.length === 2)
-	{
 
+  		
 		if (cardsInPlay[0] === cardsInPlay[1])
 		{
-			alert("You found a match!");
+			
+			score += 1;
+			alert("You found a match! and your score: " + score);
+			console.log ("Current Score: " + score);
 		}
 		else
 		{
-		alert("Sorry, try again.")
+			alert("Sorry, try again.")
 		}
-	}		
-};
+		
+		
+		//remove the two cards in cardsInPlay, only two cards at given time.
+		for (var i = 0; i<=cardsInPlay.length; i++ )
+		{
+			//console.log (i + " : "+ cardsInPlay);	
+			cardsInPlay.pop ();	
+			
+		}
+
+		
+}; // end checkMatch();
 
 //start flipcard function
-var flipcard = function (cardId){
+var flipcard = function (){
 	
-	console.log("User flipped " + cards[cardId].rank);
+	//var cardId;
+	var cardId = this.getAttribute('data-id');
 	cardsInPlay.push (cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
-	checkForMatch ();
+	console.log ("Card flipped" + cardId)
+	this.setAttribute('src',cards[cardId].cardImage);
+	if (cardsInPlay.length === 2)
+	{
+		checkForMatch ();
+	}
 	
+
 }; // end flipCard function
 
+var resetAllCard = function (){
+	
+	window.location.reload(true);
+	
+};
 
-flipcard (0);
-flipcard (2);
+var createBoard = function (){
+	var cardElement;
+	for (var i = 0; i <cards.length ; i++)
+	{
+		console.log ("in here: createBoard");
+		cardElement = document. createElement('img');
+		cardElement.setAttribute('src',"images/back.png");
+        cardElement.setAttribute('data-id',i);
+        cardElement.addEventListener('click', flipcard);
+        document.getElementById('game-board').appendChild(cardElement);
+
+        var resetCards = document.getElementsByTagName ('button')[0];
+        resetCards.addEventListener('click',resetAllCard);
+	}
+		//cardElement.setAttribute('src',"images/back.png");
+		
+};
+
+
+createBoard();
 
